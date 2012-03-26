0183c59933d3f83c7d01d68e557a208b25e4d12e
Blake Mizerany <blake.mizerany@gmail.com>
2012-03-26 15:41:31 -0700

    more debug

--- error.go
+++ error.go
@@ -62,14 +62,14 @@ func errRecover(err *error) {
 	case *PGError:
 		if v.Fatal() {
 			*err = driver.ErrBadConn
-			println("pgerror: bad conn")
+			println("pgerror: bad conn", v.Error())
 		} else {
 			*err = v
 		}
 	case error:
 		if v == io.EOF {
 			*err = driver.ErrBadConn
-			println("reg error: bad conn")
+			println("reg error: EOF")
 		} else {
 			*err = v
 		}
