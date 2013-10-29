lib/pq

commit 13601feaf94cbd2df4a9b0b752150e08c5885f72

Handle 'C' command complete message

conn.go
@@ -212,6 +212,10 @@ func (cn *conn) prepareTo(q, stmtName string) (_ driver.Stmt, err error) {
 			return st, err
 		case 'E':
 			err = parseError(r)
+		case 'C': // command complete
+			// Do we need to do anything else, perhaps depending upon the
+			// command tag?
+			return st, err
 		default:
 			errorf("unexpected describe rows response: %q", t)
 		}

commit 816d8be9a6b911233b27d3bdb08cab321f360970

Don't tolerate CommandComplete after Parse + Describe + Sync
Commit 13601feaf94cbd2df4a9b0b752150e08c5885f72 introduced this change,
(after being reported in issue #77), but the problem does not seem to
be with pq misbehaving; the test case itself is to blame.  It does not
correctly close the Rows object before attempting to operate on the
transaction again, which the "Extended query" mode does not like in
the slightest.

The TestRollback test also only tested the simpleQuery path, which
did not have a problem to begin with because it eats the
CommandComplete message from the server if the query returns no
results.  Get rid of the defective test case, and add a new one
testing an error response to the Parse message.

conn.go
@@ -426,9 +426,6 @@ func (cn *conn) prepareToSimpleStmt(q, stmtName string) (_ *stmt, err error) {
 			return st, err
 		case 'E':
 			err = parseError(r)
-		case 'C':
-			// command complete
-			return st, err
 		default:
 			errorf("unexpected describe rows response: %q", t)
 		}

conn_test.go
@@ -474,6 +474,23 @@ func TestBindError(t *testing.T) {
 	defer r.Close()
 }
 
+func TestParseErrorInExtendedQuery(t *testing.T) {
+	db := openTestConn(t)
+	defer db.Close()
+
+	rows, err := db.Query("PARSE_ERROR $1", 1)
+	if err == nil {
+		t.Fatal("expected error")
+	}
+
+	rows, err = db.Query("SELECT 1")
+	if err != nil {
+		t.Fatal(err)
+	}
+	rows.Close()
+}
+
+
 // TestReturning tests that an INSERT query using the RETURNING clause returns a row.
 func TestReturning(t *testing.T) {
 	db := openTestConn(t)
@@ -611,41 +628,6 @@ FROM (VALUES (0::integer, NULL::text), (1, 'test string')) AS t;`)
 	defer r.Close()
 
 	for r.Next() {
-	}
-}
-
-// Open transaction, issue INSERT query inside transaction, rollback
-// transaction, issue SELECT query to same db used to create the tx.  No rows
-// should be returned.
-func TestRollback(t *testing.T) {
-	db := openTestConn(t)
-	defer db.Close()
-
-	_, err := db.Exec("CREATE TEMP TABLE temp (a int)")
-	if err != nil {
-		t.Fatal(err)
-	}
-	sqlInsert := "INSERT INTO temp VALUES (1)"
-	sqlSelect := "SELECT * FROM temp"
-	tx, err := db.Begin()
-	if err != nil {
-		t.Fatal(err)
-	}
-	_, err = tx.Query(sqlInsert)
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = tx.Rollback()
-	if err != nil {
-		t.Fatal(err)
-	}
-	r, err := db.Query(sqlSelect)
-	if err != nil {
-		t.Fatal(err)
-	}
-	// Next() returns false if query returned no rows.
-	if r.Next() {
-		t.Fatal("Transaction rollback failed")
 	}
 }
 