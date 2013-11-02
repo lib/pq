lib/pq

commit b7935a1b79fa2dee944d464273356dd3ba338fc5

Fix query error handling

conn.go
@@ -276,21 +276,24 @@ func (cn *conn) simpleQuery(q string) (res driver.Rows, err error) {
 	b.string(q)
 	cn.send(b)
 
-	res = &rows{st: st}
-
 	for {
 		t, r := cn.recv1()
 		switch t {
-		case 'C':
-			// done
-			return
+		case 'C', 'N':
+			// Ignore--we may need to consume Complete
+			// here if the query finishes with an error,
+			// and NoticeResponse if we get a notice (we
+			// should communicate these rather than
+			// ignoring them, but there's not a great way
+			// to expose them right now)
 		case 'Z':
 			// done
 			return
 		case 'E':
-			st.lasterr = parseError(r)
-			return
+			res = nil
+			err = parseError(r)
 		case 'T':
+			res = &rows{st: st}
 			st.cols, st.rowTyps = parseMeta(r)
 			// After we get the meta, we want to kick out to Next()
 			return
@@ -593,10 +596,8 @@ func (st *stmt) Close() (err error) {
 	return nil
 }
 
-func (st *stmt) Query(v []driver.Value) (_ driver.Rows, err error) {
-	defer errRecover(&err)
-	st.exec(v)
-	return &rows{st: st}, nil
+func (st *stmt) Query(v []driver.Value) (r driver.Rows, err error) {
+	return st.cn.Query(st.query, v)
 }
 
 func (st *stmt) Exec(v []driver.Value) (res driver.Result, err error) {

conn_test.go
@@ -259,6 +259,16 @@ func TestNoData(t *testing.T) {
 		}
 		t.Fatal("unexpected row")
 	}
+
+	_, err = db.Query("SELECT * FROM nonexistenttable WHERE age=$1", 20)
+	if err == nil {
+		t.Fatal("Should have raised an error on non existent table")
+	}
+
+	_, err = db.Query("SELECT * FROM nonexistenttable")
+	if err == nil {
+		t.Fatal("Should have raised an error on non existent table")
+	}
 }
 
 func TestPGError(t *testing.T) {
@@ -327,18 +337,15 @@ func TestErrorOnQuery(t *testing.T) {
 
 	sql := "DO $$BEGIN RAISE unique_violation USING MESSAGE='foo'; END; $$;"
 	r, err := db.Query(sql)
-	if err != nil {
-		t.Fatal(err)
+	if r != nil {
+		t.Fatal("Should not return rows")
 	}
-	defer r.Close()
-
-	if r.Next() {
-		t.Fatal("unexpected row, want error")
+	if err == nil {
+		t.Fatal("Should have raised error")
 	}
-
-	_, ok := r.Err().(PGError)
+	_, ok := err.(PGError)
 	if !ok {
-		t.Fatalf("expected PGError, was: %#v", r.Err())
+		t.Fatalf("expected PGError, was: %#v", err)
 	}
 
 	r, err = db.Query("SELECT 1 WHERE true = false") // returns no rows

commit d153437d2a79d7fc851e0bf9ba8f8fee48adb026

Fix prepared statements
Prepared statements were inadvertently broken in commit
b7935a1b79fa2dee944d4 by making stmt.Query() re-prepare the statement
every time.

conn.go
@@ -692,7 +692,9 @@ func (st *stmt) Close() (err error) {
 }
 
 func (st *stmt) Query(v []driver.Value) (r driver.Rows, err error) {
-	return st.cn.Query(st.query, v)
+	defer errRecover(&err)
+	st.exec(v)
+	return &rows{st: st}, nil
 }
 
 func (st *stmt) Exec(v []driver.Value) (res driver.Result, err error) {