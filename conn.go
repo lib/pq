lib/pq

commit 344cc7146c482865d57b20ef84b5e6009da94f9f

Make connect_timeout apply to the entire connection procedure
As committed in 9d56fa64939666101a594570df8197, only the TCP connection
establishment part was affected by the timeout.  The new behaviour is
more intuitive and matches that of libpq's.

conn.go
@@ -168,6 +168,8 @@ func Open(name string) (_ driver.Conn, err error) {
 	cn.ssl(o)
 	cn.buf = bufio.NewReader(cn.c)
 	cn.startup(o)
+	// reset the deadline, in case one was set (see dial)
+	cn.c.SetDeadline(time.Time{})
 	return cn, nil
 }
 
@@ -185,7 +187,15 @@ func dial(o values) (net.Conn, error) {
 			return nil, fmt.Errorf("invalid value for parameter connect_timeout: %s", err)
 		}
 		duration := time.Duration(seconds) * time.Second
-		return net.DialTimeout(ntw, addr, duration)
+		// connect_timeout should apply to the entire connection establishment
+		// procedure, so set a timeout as well.  After startup() is done, it
+		// will be reset.
+		deadline := time.Now().Add(duration)
+		conn, err := net.DialTimeout(ntw, addr, duration)
+		if err != nil {
+			return nil, err
+		}
+		conn.SetDeadline(deadline)
 	}
 	return net.Dial(ntw, addr)
 }

commit 9dd136abd24227c7af773a9c32ef7eca53609202

Fix bug in dial() when connect_timeout is set
I completely broke this in commit 344cc7146c482865d57b20ef84b5e600.

conn.go
@@ -196,7 +196,8 @@ func dial(o values) (net.Conn, error) {
 		if err != nil {
 			return nil, err
 		}
-		conn.SetDeadline(deadline)
+		err = conn.SetDeadline(deadline)
+		return conn, err
 	}
 	return net.Dial(ntw, addr)
 }