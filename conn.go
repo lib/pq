lib/pq

commit 1e2ccf84caca692327146254602c580cbfd0a690

Work around a bug in QueryRow in the simple query path
In f8ffc32df8b9c5fd7d5ca1ac83 I fixed this for the extended query path
where the problem was much more prevalent, but issue #266 pointed out a
case where the simple query path is vulnerable to the same bug, which I
had missed in my original fix.

While at it, also add some more sanity checking to the conn.saveMessage*
handling.

conn.go
@@ -474,11 +474,21 @@ func (cn *conn) simpleQuery(q string) (res driver.Rows, err error) {
 		case 'E':
 			res = nil
 			err = parseError(r)
+		case 'D':
+			if res == nil {
+				errorf("unexpected DataRow in simple query execution")
+			}
+			// the query didn't fail; kick off to Next
+			cn.saveMessage(t, r)
+			return
 		case 'T':
+			if res != nil {
+				errorf("unexpected RowDescription in simple query execution")
+			}
 			res = &rows{st: st}
 			st.cols, st.rowTyps = parseMeta(r)
-			// After we get the meta, we want to kick out to Next()
-			return
+			// To work around a bug in QueryRow in Go 1.2 and earlier, wait
+			// until the first DataRow has been received.
 		default:
 			errorf("unknown response for simple query: %q", t)
 		}
@@ -624,6 +634,19 @@ func (cn *conn) send(m *writeBuf) {
 func (cn *conn) sendSimpleMessage(typ byte) (err error) {
 	_, err = cn.c.Write([]byte{typ, '\x00', '\x00', '\x00', '\x04'})
 	return err
+}
+
+// saveMessage memorizes a message and its buffer in the conn struct.
+// recvMessage will then return these values on the next call to it.  This
+// method is useful in cases where you have to see what the next message is
+// going to be (e.g. to see whether it's an error or not) but you can't handle
+// the message yourself.
+func (cn *conn) saveMessage(typ byte, buf *readBuf) {
+	if cn.saveMessageType != 0 {
+		errorf("unexpected saveMessageType %d", cn.saveMessageType)
+	}
+	cn.saveMessageType = typ
+	cn.saveMessageBuffer = buf
 }
 
 // recvMessage receives any message from the backend, or returns an error if
@@ -961,8 +984,7 @@ workaround:
 			err = parseError(r)
 		case 'C', 'D':
 			// the query didn't fail, but we can't process this message
-			st.cn.saveMessageType = t
-			st.cn.saveMessageBuffer = r
+			st.cn.saveMessage(t, r)
 			return
 		case 'Z':
 			if err == nil {

conn_test.go
@@ -520,11 +520,12 @@ func TestErrorOnQueryRowSimpleQuery(t *testing.T) {
 	}
 }
 
-// Test the QueryRow bug workaround in exec
+// Test the QueryRow bug workarounds in stmt.exec() and simpleQuery()
 func TestQueryRowBugWorkaround(t *testing.T) {
 	db := openTestConn(t)
 	defer db.Close()
 
+	// stmt.exec()
 	_, err := db.Exec("CREATE TEMP TABLE notnulltemp (a varchar(10) not null)")
 	if err != nil {
 		t.Fatal(err)
@@ -533,14 +534,87 @@ func TestQueryRowBugWorkaround(t *testing.T) {
 	var a string
 	err = db.QueryRow("INSERT INTO notnulltemp(a) values($1) RETURNING a", nil).Scan(&a)
 	if err == sql.ErrNoRows {
-		t.Errorf("expected constraint violation error; got: %v", err)
+		t.Fatalf("expected constraint violation error; got: %v", err)
 	}
 	pge, ok := err.(*Error)
 	if !ok {
-		t.Errorf("expected *Error; got: %#v", err)
+		t.Fatalf("expected *Error; got: %#v", err)
 	}
 	if pge.Code.Name() != "not_null_violation" {
-		t.Errorf("expected not_null_violation; got: %s (%+v)", pge.Code.Name(), err)
+		t.Fatalf("expected not_null_violation; got: %s (%+v)", pge.Code.Name(), err)
+	}
+
+	// Test workaround in simpleQuery()
+	tx, err := db.Begin()
+	if err != nil {
+		t.Fatalf("unexpected error %s in Begin", err)
+	}
+	defer tx.Rollback()
+
+	_, err = tx.Exec("SET LOCAL check_function_bodies TO FALSE")
+	if err != nil {
+		t.Fatalf("could not disable check_function_bodies: %s", err)
+	}
+	_, err = tx.Exec(`
+CREATE OR REPLACE FUNCTION bad_function()
+RETURNS integer
+-- hack to prevent the function from being inlined
+SET check_function_bodies TO TRUE
+AS $$
+	SELECT text 'bad'
+$$ LANGUAGE sql`)
+	if err != nil {
+		t.Fatalf("could not create function: %s", err)
+	}
+
+	err = tx.QueryRow("SELECT * FROM bad_function()").Scan(&a)
+	if err == nil {
+		t.Fatalf("expected error")
+	}
+	pge, ok = err.(*Error)
+	if !ok {
+		t.Fatalf("expected *Error; got: %#v", err)
+	}
+	if pge.Code.Name() != "invalid_function_definition" {
+		t.Fatalf("expected invalid_function_definition; got: %s (%+v)", pge.Code.Name(), err)
+	}
+
+	err = tx.Rollback()
+	if err != nil {
+		t.Fatalf("unexpected error %s in Rollback", err)
+	}
+
+	// Also test that simpleQuery()'s workaround works when the query fails
+	// after a row has been received.
+	rows, err := db.Query(`
+select
+	(select generate_series(1, ss.i))
+from (select gs.i
+      from generate_series(1, 2) gs(i)
+      order by gs.i limit 2) ss`)
+	if err != nil {
+		t.Fatalf("query failed: %s", err)
+	}
+	if !rows.Next() {
+		t.Fatalf("expected at least one result row; got %s", rows.Err())
+	}
+	var i int
+	err = rows.Scan(&i)
+	if err != nil {
+		t.Fatalf("rows.Scan() failed: %s", err)
+	}
+	if i != 1 {
+		t.Fatalf("unexpected value for i: %d", i)
+	}
+	if rows.Next() {
+		t.Fatalf("unexpected row")
+	}
+	pge, ok = rows.Err().(*Error)
+	if !ok {
+		t.Fatalf("expected *Error; got: %#v", err)
+	}
+	if pge.Code.Name() != "cardinality_violation" {
+		t.Fatalf("expected cardinality_violation; got: %s (%+v)", pge.Code.Name(), rows.Err())
 	}
 }
 

commit e94d0da13616c796fedfaeaea3a9818aee80e01a

Tolerate CommandComplete messages before the data in Simple Query
Commit 1e2ccf84caca692327146254602c580cbf inadvertently broke the case
where one or more CommandComplete messages were received before an
actual result set, as reported in #282 by rylanhalteman.  Revert to the
old behaviour of tolerating any number of CommandComplete messages
before the actual data is received.

conn.go
@@ -482,11 +482,11 @@ func (cn *conn) simpleQuery(q string) (res driver.Rows, err error) {
 			cn.saveMessage(t, r)
 			return
 		case 'T':
-			if res != nil {
-				errorf("unexpected RowDescription in simple query execution")
-			}
+			// res might be non-nil here if we received a previous
+			// CommandComplete, but that's fine; just overwrite it
 			res = &rows{st: st}
 			st.cols, st.rowTyps = parseMeta(r)
+
 			// To work around a bug in QueryRow in Go 1.2 and earlier, wait
 			// until the first DataRow has been received.
 		default:

conn_test.go
@@ -771,6 +771,25 @@ func TestIssue196(t *testing.T) {
 	}
 }
 
+// Test that any CommandComplete messages sent before the query results are
+// ignored.
+func TestIssue282(t *testing.T) {
+	db := openTestConn(t)
+	defer db.Close()
+
+	var search_path string
+	err := db.QueryRow(`
+		SET LOCAL search_path TO pg_catalog;
+		SET LOCAL search_path TO pg_catalog;
+		SHOW search_path`).Scan(&search_path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if search_path != "pg_catalog" {
+		t.Fatalf("unexpected search_path %s", search_path)
+	}
+}
+
 func TestReadFloatPrecision(t *testing.T) {
 	db := openTestConn(t)
 	defer db.Close()