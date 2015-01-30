2ca26b30a7edcd0b0af72d372c93a2d9d9c2038a
Chris Bandy <bandy.chris@gmail.com>
2015-01-30 04:54:20 +0000

    Remove workaround for golang/go#3487
    Fixed in Go 1.1

--- encode.go
+++ encode.go
@@ -150,12 +150,6 @@ func appendEscapedText(buf []byte, text string) []byte {
 func mustParse(f string, typ oid.Oid, s []byte) time.Time {
 	str := string(s)
 
-	// Special case until time.Parse bug is fixed:
-	// http://code.google.com/p/go/issues/detail?id=3487
-	if str[len(str)-2] == '.' {
-		str += "0"
-	}
-
 	// check for a 30-minute-offset timezone
 	if (typ == oid.T_timestamptz || typ == oid.T_timetz) &&
 		str[len(str)-3] == ':' {
