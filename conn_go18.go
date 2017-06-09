ea6a47944e721c48470cb3a3646c510f31c738d7
kshvakov <shvakov@gmail.com>
2017-06-09 10:57:51 +0300

    go 1.8: implement ValueConverter for smtp

--- conn_go18.go
+++ conn_go18.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"reflect"
 )
 
 // Implement the "QueryerContext" interface
@@ -125,4 +126,64 @@ func (cn *conn) cancel() error {
 		_, err := io.Copy(ioutil.Discard, c)
 		return err
 	}
+}
+
+var _converter = &converter{}
+
+func (st *stmt) ColumnConverter(idx int) driver.ValueConverter {
+	return _converter
+}
+
+type converter struct{}
+
+func (c *converter) ConvertValue(v interface{}) (driver.Value, error) {
+	if driver.IsValue(v) {
+		return v, nil
+	}
+
+	switch value := v.(type) {
+	case int:
+		return int64(value), nil
+	case int8:
+		return int64(value), nil
+	case int16:
+		return int64(value), nil
+	case int32:
+		return int64(value), nil
+	case int64:
+		return int64(value), nil
+	case uint:
+		return int64(value), nil
+	case uint8:
+		return int64(value), nil
+	case uint16:
+		return int64(value), nil
+	case uint32:
+		return int64(value), nil
+	case uint64:
+		if value >= 1<<63 {
+			return fmt.Sprintf("%d", value), nil
+		}
+		return int64(value), nil
+	case float32:
+		return float64(value), nil
+	case float64:
+		return value, nil
+	case
+		[]int, []int8, []int16, []int32, []int64,
+		[]uint, []uint8, []uint16, []uint32, []uint64,
+		[]bool, []string:
+		return Array(v).Value()
+	}
+
+	rv := reflect.ValueOf(v)
+
+	if rv.Kind() == reflect.Ptr {
+		if rv.IsNil() {
+			return nil, nil
+		}
+		return c.ConvertValue(rv.Elem().Interface())
+	}
+
+	return nil, fmt.Errorf("unsupported type %T, a %s", v, rv.Kind())
 }
