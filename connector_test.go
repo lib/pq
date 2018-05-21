f4513fcb80d2b5fd6e4f7d7de1302c12f69b2108
Leigh McCulloch <leigh@mcchouse.com>
2018-05-21 04:13:16 +0000

    Add test to exercise use with database/sql.OpenDB

--- connector_test.go
+++ connector_test.go
@@ -4,9 +4,27 @@ package pq
 
 import (
 	"context"
+	"database/sql"
 	"database/sql/driver"
 	"testing"
 )
+
+func TestNewConnector_WorksWithOpenDB(t *testing.T) {
+	name := ""
+	c, err := NewConnector(name)
+	if err != nil {
+		t.Fatal(err)
+	}
+	db := sql.OpenDB(c)
+	defer db.Close()
+	// database/sql might not call our Open at all unless we do something with
+	// the connection
+	txn, err := db.Begin()
+	if err != nil {
+		t.Fatal(err)
+	}
+	txn.Rollback()
+}
 
 func TestNewConnector_Connect(t *testing.T) {
 	name := ""
