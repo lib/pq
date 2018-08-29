acebcbcfb8d7f113ae70f80f4c233153c092dea9
Tsukatsuki Kanade <i@sukitsuki.com>
2018-08-29 13:17:25 +0800

    Add support for net.IP

---
+++ ip.go
@@ -0,0 +1,35 @@
+package pq
+
+import (
+	"database/sql/driver"
+	"fmt"
+	"net"
+)
+
+type IP net.IP
+
+func (a *IP) Scan(src interface{}) error {
+	switch x := src.(type) {
+	case []uint8:
+		*a = IP(net.ParseIP(B2S(x)))
+		return nil
+	case nil:
+		*a = nil
+		return nil
+	}
+
+	return fmt.Errorf("pq: cannot convert %T to net.IP", src)
+}
+
+func B2S(bs []uint8) string {
+	var ba []byte
+	for _, b := range bs {
+		ba = append(ba, byte(b))
+
+	}
+	return string(ba)
+}
+
+func (a IP) Value() (driver.Value, error) {
+	return net.IP(a).String(), nil
+}
