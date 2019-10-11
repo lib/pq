5281391dc30eee304b16d804a6cadf9bff8dc786
Stefano Bossi <stefano.bossi@maersk.com>
2019-10-11 21:58:07 +0200

    failing test with parametrized view

---
+++ create_view_with_parameters_test.go
@@ -0,0 +1,32 @@
+package pq
+
+import (
+	"testing"
+)
+
+func TestFoo(t *testing.T) {
+	db := openTestConn(t)
+	defer db.Close()
+
+	txn, err := db.Begin()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer txn.Rollback()
+
+	_, err = txn.Exec("CREATE VIEW foo1 AS SELECT 100") // works
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = txn.Exec("CREATE TABLE foo2 AS SELECT $1", 100) // works
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = txn.Exec("CREATE VIEW foo3 AS SELECT $1", 100) // doesn't work
+	if err != nil {
+		t.Fatal(err)
+	}
+	//			Error:      	Received unexpected error:
+	//        	            	pq: got 1 parameters but the statement requires 0
+	//        	Test:       	TestFoo
+}
