ba3651763336db9c73414435ce81834773c121f4
Till Wegmüller <toasterson@gmail.com>
2022-07-28 14:20:59 -0300

    Complete buildtags as inverse mirror of user_posix

--- user_other.go
+++ user_other.go
@@ -1,7 +1,11 @@
 // Package pq is a pure Go Postgres driver for the database/sql package.
 
-//go:build js || android || hurd || zos || !solaris || !illumos
-// +build js android hurd zos !solaris !illumos
+// For some reason the current setup with the build tags causes this version of the userCurrent to be defined twice on at least illumos 
+// (thus porbably solaris aswell) to make the exclusion explicit the build tags listed here are an inverted mirror of those used in user_posix
+// user_windows still provides the function on windows NT based platforms. 
+
+//go:build js || android || hurd || zos || !aix || !darwin || !dragonfly || !freebsd || !nacl || !netbsd || !openbsd || !plan9 || !solaris || !rumprun || !illumos
+// +build js android hurd zos !aix !darwin !dragonfly !freebsd !nacl !netbsd !openbsd !plan9 !solaris !rumprun !illumos
 
 package pq
 
