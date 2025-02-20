53f2cbe932a8d382da416b12cb8c0ec6de0e3031
Giau. Tran Minh <12751435+giautm@users.noreply.github.com>
2025-02-20 23:42:52 +0700

    pg: fixed build with `wasip1`

--- user_other.go
+++ user_other.go
@@ -1,7 +1,7 @@
 // Package pq is a pure Go Postgres driver for the database/sql package.
 
-//go:build js || android || hurd || zos
-// +build js android hurd zos
+//go:build js || android || hurd || zos || wasip1
+// +build js android hurd zos wasip1
 
 package pq
 
