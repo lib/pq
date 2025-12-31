02ec27365cb77358dcbf44836ec36bc12c84fc96
Martin Tournoij <martin@arp242.net>
2025-12-31 10:10:33 +0000

    Alias NullTime to sql.NullTime
    And mark as deprecated.
    
    Fixes #902

--- encode.go
+++ encode.go
@@ -2,7 +2,7 @@ package pq
 
 import (
 	"bytes"
-	"database/sql/driver"
+	"database/sql"
 	"encoding/binary"
 	"encoding/hex"
 	"errors"
@@ -609,24 +609,9 @@ func encodeBytea(serverVersion int, v []byte) (result []byte) {
 	return result
 }
 
-// NullTime represents a time.Time that may be null. NullTime implements the
-// sql.Scanner interface so it can be used as a scan destination, similar to
-// sql.NullString.
-type NullTime struct {
-	Time  time.Time
-	Valid bool // Valid is true if Time is not NULL
-}
-
-// Scan implements the Scanner interface.
-func (nt *NullTime) Scan(value any) error {
-	nt.Time, nt.Valid = value.(time.Time)
-	return nil
-}
-
-// Value implements the driver Valuer interface.
-func (nt NullTime) Value() (driver.Value, error) {
-	if !nt.Valid {
-		return nil, nil
-	}
-	return nt.Time, nil
-}
+// NullTime represents a [time.Time] that may be null.
+// NullTime implements the [sql.Scanner] interface so
+// it can be used as a scan destination, similar to [sql.NullString].
+//
+// Deprecated: this is an alias for [sql.NullTime].
+type NullTime = sql.NullTime
