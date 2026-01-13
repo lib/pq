cefe21e13d27f5df9ac6c570098e58e10d56ad24
Martin Tournoij <martin@arp242.net>
2026-01-13 12:57:12 +0000

    Use "user" environment variable instead of "USER" on Plan 9
    Ref: #744

--- internal/pqutil/user_posix.go
+++ internal/pqutil/user_posix.go
@@ -5,10 +5,15 @@ package pqutil
 import (
 	"os"
 	"os/user"
+	"runtime"
 )
 
 func User() (string, error) {
-	if n := os.Getenv("USER"); n != "" {
+	env := "USER"
+	if runtime.GOOS == "plan9" {
+		env = "user"
+	}
+	if n := os.Getenv(env); n != "" {
 		return n, nil
 	}
 
