c9320c42e8721adae182adf2ee3eda3708844ac8
Martin Tournoij <martin@arp242.net>
2026-01-29 04:23:35 +0000

    Fix build on Windows and Plan9
    Guess that didn't quite get renamed right and since CI is only tested on
    Linux I never noticed.
    
    Fixes #1251

--- internal/pqutil/user_windows.go
+++ internal/pqutil/user_windows.go
@@ -7,14 +7,14 @@ import (
 	"syscall"
 )
 
-// Perform Windows user name lookup identically to libpq.
-//
-// The PostgreSQL code makes use of the legacy Win32 function GetUserName, and
-// that function has not been imported into stock Go. GetUserNameEx is available
-// though, the difference being that a wider range of names are available.  To
-// get the output to be the same as GetUserName, only the base (or last)
-// component of the result is returned.
 func User() (string, error) {
+	// Perform Windows user name lookup identically to libpq.
+	//
+	// The PostgreSQL code makes use of the legacy Win32 function GetUserName,
+	// and that function has not been imported into stock Go. GetUserNameEx is
+	// available though, the difference being that a wider range of names are
+	// available.  To get the output to be the same as GetUserName, only the
+	// base (or last) component of the result is returned.
 	var (
 		name     = make([]uint16, 128)
 		pwnameSz = uint32(len(name)) - 1
