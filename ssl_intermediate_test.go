3a09bf3a65602805d0c9b245145539f49147e3df
Martin Tournoij <martin@arp242.net>
2026-03-09 13:35:48 +0000

    Capture loop variable
    This wasn't changed until Go 1.22, and pq works with Go 1.21

--- ssl_intermediate_test.go
+++ ssl_intermediate_test.go
@@ -56,6 +56,7 @@ func TestSSLClientCertificateIntermediate(t *testing.T) {
 	}
 
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 			db, err := openSSLConn(t, tt.connect)
